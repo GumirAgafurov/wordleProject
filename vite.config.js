0ee22aba284cb856bbbe05d7a696b72da63f7d49
GumirYo <agafurov.gumir@gmail.com>
2026-01-15 14:23:17 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/GumirAgafurov/wordleProject/', 
+  base: '/wordleProject/', 
   build: {
     outDir: 'dist',
     sourcemap: false,
